f41a61eea0ee2c33838bde1abbb98e98879687e8
Sadab <sadabshiper@iut-dhaka.edu>
2024-01-01 23:13:29 +0600

    second commit

---
+++ 11. Tuples.py
@@ -0,0 +1,5 @@
+coordinates = (4, 6)
+
+# you can't modify tuple, which means you can't change/add/remove elements to a tuple
+
+print(coordinates[0])
---
+++ 12. Functions.py
@@ -0,0 +1,4 @@
+def ok ():
+    print("hello")
+    
+ok()
